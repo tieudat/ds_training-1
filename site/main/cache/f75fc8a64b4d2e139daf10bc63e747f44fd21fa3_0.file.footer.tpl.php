415dffbc57d79e4eb0a6a9096e3d006cae052f46
Luc Tieu <luctv2589@gmail.com>
2020-12-29 10:05:38 +0700

    remove cache

---
+++ site/main/cache/f75fc8a64b4d2e139daf10bc63e747f44fd21fa3_0.file.footer.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-12-29 09:51:50
+  from 'C:\xampp\htdocs\ds_training\site\main\view\includes\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5fea99c6b48dc1_71097093',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f75fc8a64b4d2e139daf10bc63e747f44fd21fa3' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\ds_training\\site\\main\\view\\includes\\footer.tpl',
+      1 => 1609210294,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5fea99c6b48dc1_71097093 (Smarty_Internal_Template $_smarty_tpl) {
+?><footer id="footer">
+</div><?php }
+}
